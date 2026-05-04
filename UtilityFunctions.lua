47b0ec932db7cea82832f882cab5ba49296cb31d
Traxxy1 <84944989+Traxxy1@users.noreply.github.com>
2026-05-03 20:15:15 -0500

    Update UtilityFunctions.lua

--- UtilityFunctions.lua
+++ UtilityFunctions.lua
@@ -1,26 +1,141 @@
-local utils = {}
+local Utils = {}
+local UserInputService = game:GetService("UserInputService")
+local rawwtvpp = workspace.CurrentCamera.WorldToViewportPoint
+local rawffc = workspace.FindFirstChild
+local rawGetPlayers = game.Players.GetPlayers
 
-function utils.ensureCharacterLoaded(player)
-    if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") and player.Character:FindFirstChild("Head") then
+local services = {
+    VirtualInputManager = Instance.new("VirtualInputManager")
+}
+
+setmetatable(services, {
+    __index = function(self, idx)
+        self[idx] = cloneref(game:GetService(idx))
+        return self[idx]
+    end,
+})
+
+function Utils.EnsureCharacterLoaded(player)
+    if player and player.Character and player.Character:FindFirstChild("Head") and player.Character:FindFirstChild("HumanoidRootPart") and player.Character:FindFirstChild("Humanoid") then
         return true
     else
         return false
     end
 end
 
-function utils.getHumanoidRootPart(player)
-    if utils.ensureCharacterLoaded(player) then
+function Utils.GetHumanoidRootPart(player)
+    if Utils.EnsureCharacterLoaded(player) then
         return player.Character.HumanoidRootPart
     else
+        warn("GetHumanoidRootPart - Player not loaded")
         return
     end
 end
 
-function utils.addConnection(signal, func)
-    if not signal or not func then return end
-    if typeof(signal) ~= "RBXScriptSignal" or typeof(func) ~= "function" then return end
+function Utils.DoMouseClick(x, y)
+    if typeof(x) ~= "number" or typeof(y) ~= "number" then
+        warn("DoMouseClick expects two numbers")
+        return
+    end
+    services.VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, game, 1)
+    task.wait()
+    services.VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, game, 1)
+end
 
+function Utils.CalculateDistance(x, y)
+    local typeX = typeof(x)
+    local typeY = typeof(y)
+
+    local isVector3Pair = typeX == "Vector3" and typeY == "Vector3"
+    local isVector2Pair = typeX == "Vector2" and typeY == "Vector2"
+
+    if not (isVector3Pair or isVector2Pair) then
+        warn("CalculateDistance expects two Vector3s or two Vector2s, got:", typeX, "and", typeY)
+        return
+    end
+
+    return (x - y).Magnitude
+end
+
+
+function Utils.AddInputListener(keycode, func)
+    if not keycode or not func then return end
+    if typeof(keycode) ~= "EnumItem" or keycode.EnumType ~= Enum.KeyCode then
+        warn("AddInputListener expects Keycode, you gave:", tostring(typeof(keycode)))
+        return
+    end
+    if typeof(func) ~= "function" then
+        warn("AddInputListener expects function, you gave:", tostring(typeof(func)))
+        return
+    end
+    
+    return UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+        if gameProcessedEvent then return end
+
+        if input.KeyCode == keycode then
+            func()
+        end
+    end)
+end
+
+function Utils.AddConnection(signal, func)
+    if typeof(signal) ~= "RBXScriptSignal" then
+        warn("AddConnection expects signal, you gave:", tostring(typeof(signal)))
+        return
+    end
+    if typeof(func) ~= "function" then
+        warn("AddConnection expects function, you gave:", tostring(typeof(func)))
+        return
+    end
     return signal:Connect(func)
 end
 
-return utils
+function Utils.GetClosestPlayer(position, teamName, child)
+    if typeof(position) ~= "Vector3" then
+        warn("GetClosestPlayer expects Vector3, you gave:", tostring(typeof(position)))
+        return
+    end
+    local plr = game:GetService("Players").LocalPlayer
+    local closestPlayer
+    local shortestDistance = math.huge
+
+    for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+        if player and player ~= plr and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+            if teamName == nil or (player.Team and player.Team.Name == teamName) then
+                if child == nil or (player:FindFirstChild(child)) then
+                    local distance = Utils.CalculateDistance(position, player.Character.HumanoidRootPart.Position)
+                    if distance < shortestDistance then
+                        shortestDistance = distance
+                        closestPlayer = player
+                    end
+                end
+            end
+        end
+    end
+    return closestPlayer
+end
+
+function Utils.GetClosestPlayerToCrosshair()
+    local screenCenter = workspace.CurrentCamera.ViewportSize / 2
+    local closestPlayer
+    local shortestDistance = math.huge
+
+    for _, player in pairs(rawGetPlayers(game.Players)) do
+        if player and player ~= game.Players.LocalPlayer and player.Character and rawffc(player.Character, "HumanoidRootPart") then
+            local playerHead = rawffc(player.Character, "Head")
+            if playerHead then
+                local headPos, onScreen = rawwtvpp(workspace.CurrentCamera, playerHead.Position)
+                if headPos and onScreen then
+                    local distance = ((Vector2.new(headPos.X, headPos.Y)) - screenCenter).Magnitude
+                    if distance < shortestDistance then
+                        closestPlayer = player
+                        shortestDistance = distance
+                    end
+                end
+            end
+        end
+    end
+    return closestPlayer
+end
+
+return Utils
